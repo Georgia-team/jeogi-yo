0ef1434700e31f339a3ef79cb5c61f8bbd7ae22d
jjangjun0 <junyoung5915@gmail.com>
2026-07-08 12:01:27 +0900

    Category Entity 1차 코딩

--- src/main/java/com/georgia/jeogiyo/category/entity/Category.java
+++ src/main/java/com/georgia/jeogiyo/category/entity/Category.java
@@ -1,5 +1,33 @@
 package com.georgia.jeogiyo.category.entity;
 
+import com.georgia.jeogiyo.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Category {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category extends BaseEntity {
+    // PK, 카테고리 고유 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "category_id", nullable = false, updatable = false)
+    private UUID categoryId;
+    // 카테고리 이름
+    @Column(name = "category_name", nullable = false, unique = true, length = 50)
+    private String categoryName;
+
+    public Category(String categoryName, String createdBy) {
+        this.categoryName = categoryName;
+        // this.createdBy = createdBy;
+        // this.createdAt = LocalDateTime.now();
+    }
 }
