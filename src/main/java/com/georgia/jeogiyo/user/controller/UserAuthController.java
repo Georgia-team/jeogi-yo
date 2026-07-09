e9f3546dcff52f1054340d111bdefb00030c5ac8
Seo-JS0823 <seong0093571@naver.com>
2026-07-09 12:21:43 +0900

    feat: 회원가입 컨트롤러 완료

--- src/main/java/com/georgia/jeogiyo/user/controller/UserAuthController.java
+++ src/main/java/com/georgia/jeogiyo/user/controller/UserAuthController.java
@@ -2,13 +2,14 @@ package com.georgia.jeogiyo.user.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.georgia.jeogiyo.user.dto.request.UserSignupRequest;
 import com.georgia.jeogiyo.user.dto.response.UserLoginResponse;
 import com.georgia.jeogiyo.user.dto.response.UserSignupResponse;
 import com.georgia.jeogiyo.user.service.UserService;
-import com.georgia.jeogiyo.user.service.UserFinder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,12 +20,12 @@ public class UserAuthController {
 	
 	private final UserService userCommandService;
 	
-	private final UserFinder userFinder;
-	
 	@PostMapping("/signup")
-	public ResponseEntity<UserSignupResponse> signup() {
+	public ResponseEntity<UserSignupResponse> signup(@RequestBody UserSignupRequest userSignup) {
 		// TODO: 공통 응답 객체 완료되면 반환 타입 바꿀 예정
-		return null;
+		UserSignupResponse signupUser = userCommandService.signup(userSignup);
+		
+		return ResponseEntity.ok(signupUser);
 	}
 	
 	@PostMapping("/login")
