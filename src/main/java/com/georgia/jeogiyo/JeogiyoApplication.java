6f10b76d752d5a08e2d0fdc05221526f24ec6528
Jinhyelim <254581953+Jinhyelim@users.noreply.github.com>
2026-07-10 14:10:57 +0900

    SecurityAuditorAware 작성, BaseEntity 수정

--- src/main/java/com/georgia/jeogiyo/JeogiyoApplication.java
+++ src/main/java/com/georgia/jeogiyo/JeogiyoApplication.java
@@ -2,10 +2,9 @@ package com.georgia.jeogiyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class JeogiyoApplication {
 
--- src/main/java/com/georgia/jeogiyo/global/security/SecurityConfig.java
+++ src/main/java/com/georgia/jeogiyo/global/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용
-                        .requestMatchers("api/v1/auth/**").permitAll() // 'api/v1/auth/'로 시작하는 요청 모두 접근 허가 (회원가입, 로그인)
+                        .requestMatchers("/api/v1/auth/**").permitAll() // 'api/v1/auth/'로 시작하는 요청 모두 접근 허가 (회원가입, 로그인)
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
