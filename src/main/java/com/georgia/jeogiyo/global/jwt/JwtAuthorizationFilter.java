db9ba2bbf42f5f4d17f07c72295caae813e91951
Jinhyelim <254581953+Jinhyelim@users.noreply.github.com>
2026-07-10 11:18:25 +0900

    JwtAuthorizationFilter 생성

---
+++ src/main/java/com/georgia/jeogiyo/global/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,78 @@
+package com.georgia.jeogiyo.global.jwt;
+
+import com.georgia.jeogiyo.global.security.UserDetailsServiceImpl;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * ──────────────────────────────────────────────────────────────────────────────────────────────────
+ * 작성자: 진혜림
+ * 작성일: 2026-07-10
+ * 설명: JWT 검증 및 인가, 인증 객체 생성 -> 인증 처리
+ * ──────────────────────────────────────────────────────────────────────────────────────────────────
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenValue = jwtUtil.getTokenFromRequest(request);
+
+        if (StringUtils.hasText(tokenValue)) {
+            // JWT 토큰 substring
+            tokenValue = jwtUtil.subStringToken(tokenValue);
+
+            // JWT 토큰 검증 (위변조 & 만료 검증) 실패시 에러
+            if (!jwtUtil.validateToken(tokenValue)) {
+                log.error("Token Error");
+                return;
+            }
+
+            // 토큰에서 사용자 정보 가져오기
+            Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
+
+            try {
+                setAuthentication(info.getSubject());
+            } catch (Exception e) {
+                log.error("Authentication Error");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // 인증 처리
+    public void setAuthentication(String LoginId) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(LoginId);
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+    }
+
+    // 인증 객체 생성
+    private Authentication createAuthentication(String LoginId) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(LoginId);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+}
