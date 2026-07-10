7de0062ef05d9d1ef8d22fb10b9fd89d144716ee
Seo-JS0823 <seong0093571@naver.com>
2026-07-10 18:45:47 +0900

    test: 회원 목록 검색 Master 권한 테스트 추가

--- src/test/java/com/georgia/jeogiyo/user/controller/UserSearchApiTest.java
+++ src/test/java/com/georgia/jeogiyo/user/controller/UserSearchApiTest.java
@@ -1,8 +1,11 @@
 package com.georgia.jeogiyo.user.controller;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,11 +16,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.georgia.jeogiyo.user.dto.request.UserLoginRequest;
+import com.georgia.jeogiyo.user.dto.request.UserSearchRequest;
 import com.georgia.jeogiyo.user.dto.request.UserSignupRequest;
 import com.georgia.jeogiyo.user.dto.response.UserLoginResponse;
 import com.georgia.jeogiyo.user.entity.Role;
@@ -99,6 +105,72 @@ public class UserSearchApiTest {
 		.andExpect(jsonPath("$.phone").value(user.getPhone()))
 		.andExpect(jsonPath("$.email").value(user.getEmail()))
 		.andExpect(jsonPath("$.role").value(user.getRole().name()))
+		;
+	}
+	
+	@Test
+	@DisplayName("API: 회원 목록 검색")
+	void searchUserListTest() throws Exception {
+		List<UserSignupRequest> testUserListNameTest = List.of(
+				new UserSignupRequest("test01", "Password01@", "nickname01", "02-000-0000", "test01@email.com"),
+				new UserSignupRequest("test02", "Password01@", "nickname02", "02-000-0000", "test02@email.com"),
+				new UserSignupRequest("test03", "Password01@", "nickname03", "02-000-0000", "test03@email.com"),
+				new UserSignupRequest("test04", "Password01@", "nickname04", "02-000-0000", "test04@email.com"),
+				new UserSignupRequest("test05", "Password01@", "nickname05", "02-000-0000", "test05@email.com"),
+				new UserSignupRequest("test06", "Password01@", "nickname06", "02-000-0000", "test06@email.com"),
+				new UserSignupRequest("test07", "Password01@", "nickname07", "02-000-0000", "test07@email.com"),
+				new UserSignupRequest("test08", "Password01@", "nickname08", "02-000-0000", "test08@email.com"),
+				new UserSignupRequest("test09", "Password01@", "nickname09", "02-000-0000", "test09@email.com"),
+				new UserSignupRequest("test10", "Password01@", "nickname10", "02-000-0000", "test10@email.com"),
+				new UserSignupRequest("test11", "Password01@", "nickname11", "02-000-0000", "test11@email.com"),
+				new UserSignupRequest("test12", "Password01@", "nickname12", "02-000-0000", "test12@email.com")
+		);
+		testUserListNameTest.stream()
+		.forEach(u -> {
+			userService.signup(u);
+		});
+		
+		em.flush();
+		em.clear();
+		
+		String url = "/api/v1/users";
+		
+		UserLoginRequest userLoginRequest = new UserLoginRequest(
+				userSignupRequest.getLoginId(),
+				userSignupRequest.getPassword()
+		);
+		
+		UserLoginResponse loginResponse = userService.login(userLoginRequest);
+		
+		UserSearchRequest searchRequest = new UserSearchRequest();
+		
+		searchRequest.setPage(0);
+		searchRequest.setSize(10);
+		searchRequest.setRole(Role.CUSTOMER);
+		searchRequest.setKeyword("test");
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("role", searchRequest.getRole().name());
+		params.add("keyword", searchRequest.getKeyword());
+		params.add("page", String.valueOf(searchRequest.getPage()));
+		params.add("size", String.valueOf(searchRequest.getSize()));
+		
+		mockMvc
+		.perform(get(url)
+				.contentType(MediaType.APPLICATION_JSON)
+				.params(params)
+		)
+		.andExpect(status().isForbidden())
+		;
+		
+		mockMvc
+		.perform(get(url)
+				.cookie(new Cookie("Authorization", loginResponse.getAccessToken()))
+				.contentType(MediaType.APPLICATION_JSON)
+				.params(params)
+		)
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$", hasSize(10)))
 		;
 	}
 	
