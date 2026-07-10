270b58137c3fee32004c1ecb0d1ca39a8ef4af88
Jinhyelim <254581953+Jinhyelim@users.noreply.github.com>
2026-07-10 14:57:01 +0900

    BaseEntity, JwtAuthorizationFilter Modify

--- src/main/java/com/georgia/jeogiyo/JeogiyoApplication.java
+++ src/main/java/com/georgia/jeogiyo/JeogiyoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditingConfig")
+@EnableJpaAuditing(auditorAwareRef = "auditingConfig", modifyOnCreate = false)
 @SpringBootApplication
 public class JeogiyoApplication {
 
