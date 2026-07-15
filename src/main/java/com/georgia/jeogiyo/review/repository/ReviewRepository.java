ed12dd3a078c5608572d87ab983260dff9b1b77d
jjangjun0 <junyoung5915@gmail.com>
2026-07-15 14:08:42 +0900

    modify: Store에서 활용할 count 함수

--- src/main/java/com/georgia/jeogiyo/review/repository/ReviewRepository.java
+++ src/main/java/com/georgia/jeogiyo/review/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.georgia.jeogiyo.review.repository;
 
 import com.georgia.jeogiyo.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -18,4 +20,14 @@ public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRep
 
     // 주문에 이미 리뷰가 존재하는지 확인 (삭제되지 않은 리뷰만 존재 여부를 확인)
     boolean existsByOrder_OrderIdAndIsDeletedFalse(UUID orderId);
+
+    long countByStore_StoreIdAndIsDeletedFalse(UUID storeId);
+
+    @Query("""
+        select coalesce(avg(r.rating), 0.0)
+        from Review r
+        where r.store.storeId = :storeId
+          and r.isDeleted = false
+        """)
+    Double findAverageRatingByStoreId(@Param("storeId") UUID storeId);
 }
