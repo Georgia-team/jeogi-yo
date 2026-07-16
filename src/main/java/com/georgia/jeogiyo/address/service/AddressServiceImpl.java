6a376d39802b42e0f32c5dbc041f8e8d89a21f0c
Seo-JS0823 <seong0093571@naver.com>
2026-07-16 10:36:48 +0900

    refactor: 서비스 지역 공통 유틸 적용

--- src/main/java/com/georgia/jeogiyo/address/service/AddressServiceImpl.java
+++ src/main/java/com/georgia/jeogiyo/address/service/AddressServiceImpl.java
@@ -1,12 +1,9 @@
 package com.georgia.jeogiyo.address.service;
 
-import java.util.List;
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.georgia.jeogiyo.address.dto.request.AddressCreateRequest;
 import com.georgia.jeogiyo.address.dto.request.AddressUpdateRequest;
@@ -17,6 +14,7 @@ import com.georgia.jeogiyo.address.entity.Address;
 import com.georgia.jeogiyo.address.repository.AddressRepository;
 import com.georgia.jeogiyo.global.exception.BusinessException;
 import com.georgia.jeogiyo.global.exception.GlobalErrorCode;
+import com.georgia.jeogiyo.global.util.DeliveryAreaValidator;
 import com.georgia.jeogiyo.user.entity.User;
 import com.georgia.jeogiyo.user.service.UserFinder;
 
@@ -33,18 +31,12 @@ public class AddressServiceImpl implements AddressService {
 	
 	private final UserFinder userFinder;
 	
-	private static final String ROAD_ADDRESS_PREFIX = "서울특별시 종로구 "; 
-	
-	private static final List<String> DELIVERY_AREA_ROADS = List.of(
-			"세종대로", "새문안로", "종로1길", "종로3길", "사직로", "율곡로", "자하문로", "우정국로", "삼청로", "경희궁길", "경희궁1길"
-	);
-	
 	// 배송지 등록
 	@Override
 	public AddressCreateResponse addressCreate(String loginId, AddressCreateRequest addressCreate) {
 		User user = userFinder.getUserByLoginId(loginId);
 		
-		validateAllowedArea(addressCreate.getRoadAddress());
+		DeliveryAreaValidator.validate(addressCreate.getRoadAddress());
 		
 		if(addressCreate.getIsDefault() == true) {
 			Address defaultAddress = addressFinder.findByUserAndDefault(user)
@@ -69,6 +61,10 @@ public class AddressServiceImpl implements AddressService {
 		User user = userFinder.getUserByLoginId(loginId);
 		
 		Address address = addressFinder.findByUserAndAddressId(user, UUID.fromString(addressId));
+		
+		if(addressUpdate.getRoadAddress() != null) {
+			DeliveryAreaValidator.validate(addressUpdate.getRoadAddress());
+		}
 		
 		if(!address.isDefault() && addressUpdate.getIsDefault()) {
 			Address defaultAddress = addressFinder.findByUserAndDefault(user)
@@ -101,23 +97,6 @@ public class AddressServiceImpl implements AddressService {
 		address.softDelete(user.getLoginId());
 		
 		return AddressDeleteResponse.of(address);
-	}
-	
-	private void validateAllowedArea(String roadAddress) {
-		String normalizedRoadAddress = roadAddress.replace(" ", "");
-		String normalizedPrefix = ROAD_ADDRESS_PREFIX.replace(" ", "");
-		
-		if (!normalizedRoadAddress.startsWith(normalizedPrefix)) {
-			throw new BusinessException(GlobalErrorCode.OUT_OF_SERVICE_AREA);
-	  }
-		
-		boolean isAllowedArea = DELIVERY_AREA_ROADS.stream()
-				.map(road -> road.replace(" ", ""))
-				.anyMatch(normalizedRoadAddress::contains);
-		
-		if(!isAllowedArea) {
-			throw new BusinessException(GlobalErrorCode.OUT_OF_SERVICE_AREA);
-		}
 	}
 	
 }
